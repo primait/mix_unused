d1e577d270f0b2b674b6bfd32fdb2615adfc310a
korialis <thomasalderighi@gmail.com>
2022-05-23 11:38:16 +0200

    refactor transitively_unused check

--- lib/mix_unused/analyzers/unreachable.ex
+++ lib/mix_unused/analyzers/unreachable.ex
@@ -23,13 +23,30 @@ defmodule MixUnused.Analyzers.Unreachable do
     called_at_compile_time = Calls.called_at_compile_time(data, exports)
 
     for {mfa, _meta} = call <- exports,
+        report_transitively_unused?(config, graph, mfa),
         candidate?(call),
         mfa not in usages,
         mfa not in reachables,
         mfa not in called_at_compile_time,
-        config.report_transitively_used or Graph.in_degree(graph, mfa) == 0,
         into: %{},
         do: call
+  end
+
+  @spec report_transitively_unused?(Config.t(), Graph.t(), mfa()) :: boolean()
+  defp report_transitively_unused?(
+         %Config{report_transitively_unused: true},
+         _graph,
+         _mfa
+       ),
+       do: true
+
+  # when report_transitively_unused is false, only check roots of the call-graph
+  defp report_transitively_unused?(
+         %Config{report_transitively_unused: false},
+         graph,
+         mfa
+       ) do
+    Graph.in_degree(graph, mfa) == 0
   end
 
   # Clause to detect an unused struct (it is generated)
--- lib/mix_unused/analyzers/unreachable/config.ex
+++ lib/mix_unused/analyzers/unreachable/config.ex
@@ -6,12 +6,12 @@ defmodule MixUnused.Analyzers.Unreachable.Config do
   @type t :: %Config{
           usages: [module() | mfa()],
           usages_discovery: [module()],
-          report_transitively_used: boolean()
+          report_transitively_unused: boolean()
         }
 
   defstruct usages: [],
             usages_discovery: [],
-            report_transitively_used: false
+            report_transitively_unused: false
 
   @spec cast(Enum.t()) :: Config.t()
   def cast(map) do
