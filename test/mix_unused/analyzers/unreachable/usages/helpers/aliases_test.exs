a1d6d3ca8d55ef354bc8a46be0868fb639c88a2f
korialis <thomasalderighi@gmail.com>
2022-05-24 17:27:10 +0200

    directly quote code snippets in aliases tests

--- test/mix_unused/analyzers/unreachable/usages/helpers/aliases_test.exs
+++ test/mix_unused/analyzers/unreachable/usages/helpers/aliases_test.exs
@@ -1,34 +1,39 @@
 defmodule MixUnused.Analyzers.Unreachable.Usages.Helpers.AliasesTest do
-  @moduledoc """
-  """
   use ExUnit.Case
 
   alias MixUnused.Analyzers.Unreachable.Usages.Helpers.Aliases
 
   test "it is able to resolve simple aliases" do
-    aliases = Aliases.new(Code.string_to_quoted!(~s[
-      defmodule A.B.C do
-        alias A
-        alias A.B
-        alias A.B.C
-      end
-    ]))
+    aliases =
+      Aliases.new(
+        quote do
+          defmodule A.B.C do
+            alias A
+            alias A.B
+            alias A.B.C
+          end
+        end
+      )
 
     assert Aliases.resolve(aliases, [:A]) == A
     assert Aliases.resolve(aliases, [:B]) == A.B
     assert Aliases.resolve(aliases, [:C]) == A.B.C
     assert Aliases.resolve(aliases, [:B, :C]) == A.B.C
     assert Aliases.resolve(aliases, [:A, :D]) == A.D
+    assert Aliases.resolve(aliases, [:C, :D]) == A.B.C.D
   end
 
   test "it is able to resolve :as aliases" do
-    aliases = Aliases.new(Code.string_to_quoted!(~s[
-      defmodule A.B.C do
-        alias A
-        alias A.B, as: Foo
-        alias A.B.C
-      end
-    ]))
+    aliases =
+      Aliases.new(
+        quote do
+          defmodule A.B.C do
+            alias A
+            alias A.B, as: Foo
+            alias A.B.C
+          end
+        end
+      )
 
     assert Aliases.resolve(aliases, [:A]) == A
     assert Aliases.resolve(aliases, [:B]) == B
@@ -39,7 +44,11 @@ defmodule MixUnused.Analyzers.Unreachable.Usages.Helpers.AliasesTest do
   end
 
   test "resolving on an empty ast returns the name as is" do
-    aliases = Aliases.new(Code.string_to_quoted!(~s[]))
+    aliases =
+      Aliases.new(
+        quote do
+        end
+      )
 
     assert Aliases.resolve(aliases, [:A]) == A
     assert Aliases.resolve(aliases, [:B]) == B
@@ -49,7 +58,12 @@ defmodule MixUnused.Analyzers.Unreachable.Usages.Helpers.AliasesTest do
   end
 
   test "a resolve on an empty atom list returns Elixir" do
-    aliases = Aliases.new("")
+    aliases =
+      Aliases.new(
+        quote do
+        end
+      )
+
     assert Aliases.resolve(aliases, []) == Elixir
   end
 end
